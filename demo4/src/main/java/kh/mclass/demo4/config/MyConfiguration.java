bbf86b46653373640e087cee93257e6cea5c9d6d
seojw0730 <seojw0730@naver.com>
2024-06-10 19:58:48 +0900

    demo4 SNS로그인 테스트 파일 삭제

--- demo4/src/main/java/kh/mclass/demo4/config/MyConfiguration.java
+++ demo4/src/main/java/kh/mclass/demo4/config/MyConfiguration.java
@@ -14,7 +14,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import kh.mclass.demo4.common.LoginInterceptor;
-import kh.mclass.demo4.login.controller.KakaoApi;
 
 //WebMvcConfigurer -> url 관련 설정
 @Configuration
